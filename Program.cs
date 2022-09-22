f9236ede93881f5cd369cb32d710ace6e4e1bf67
BhavanaSVastrad <bhavanasvastrad@gmail.com>
2022-09-22 16:00:09 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;//namespace-collection of classses->colllection of functions/methods
+//user-defined namespace
+namespace Basic_Concepts
+    ///<summary>
+    /// basic c-sharp concepts
+    /// </summary>
+{
+    class Program
+    {
+        /// <summary>
+        /// entry point ,static-invoked once,void-returns nothing
+        /// </summary>
+
+
+
+        public static void Main(String[] args)
+        {
+            //console->classs
+            Console.WriteLine("welcome to c-sharp");
+        }
+    }
+}
