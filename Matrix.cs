41f1a427e61b5e366666cf0f9853002ce029cbd5
BhavanaSVastrad <bhavanasvastrad@gmail.com>
2022-09-29 20:09:25 +0530

    commit

---
+++ Matrix.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T_CSharp
+{
+    internal class Matrix
+    {
+        public static void Main()
+        {
+            Array2daddition();
+        }
+        public static void Array2daddition()
+        {
+            Console.WriteLine("---------------Matrix Addition------------");
+            Console.Write("Enter Rows: ");
+            int row = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Columns: ");
+            int col = Convert.ToInt32(Console.ReadLine());
+            int[,] a = new int[row, col];
+            Console.WriteLine();
+            Console.WriteLine("--------------------------------------------");
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    Console.Write("Enter First Matrix({0},{1}): ", i, j);
+                    a[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("--------------------------------------------");
+            int[,] b = new int[row, col];
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    
+                    Console.Write("Enter Second Matrix({0},{1}): ", i, j);
+                    b[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("--------------------------------------------");
+            Console.Write("\nThe First matrix is :\n");
+            for ( int i = 0; i < row; i++)
+            {
+                Console.Write("\n");
+                for (int j = 0; j < col; j++)
+                    Console.Write("{0} ", a[i, j]);
+            }
+            Console.WriteLine();
+            Console.WriteLine("--------------------------------------------");
+            Console.Write("\nThe Second matrix is :\n");
+            for (int i = 0; i < row; i++)
+            {
+                Console.Write("\n");
+                for (int j = 0; j < col; j++)
+                    Console.Write("{0} ", b[i, j]);
+            }
+         
+            int[,] result = new int[row, col];
+            Console.WriteLine();
+            Console.WriteLine("--------------------------------------------");
+            Console.WriteLine("The Addition of two matrix is:");
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    result[i, j] = a[i, j] + b[i, j];
+
+                    Console.Write(result[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("--------------------------------------------");
+        }
+    }
+}
---
+++ outkeyword.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T_CSharp
+{
+    internal class outkeyword
+    {
+        public static void Main()
+        {
+            int id;
+            string name, dept;
+            studentdetails(out id, out name, out dept);
+            Console.WriteLine("------Display Student Details using out keyword-----\n");
+            Console.WriteLine($"Student ID: {id}");
+            Console.WriteLine($"Student Name: {name}");
+            Console.WriteLine($"Student Department: {dept} \n");
+            Console.WriteLine("-----------------------------------------------------");
+
+        }
+        public static void studentdetails(out int sid, out string sname, out string sdept)
+        {
+            sid = 100;
+            sname = "BHAVANA S VASTRAD";
+            sdept = "COMPUTER SCIENCE ENGINEERING";
+        }
+    }
+}
