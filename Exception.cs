7e67c2c08975152cff24596142fe2f91c3d85159
BhavanaSVastrad <bhavanasvastrad@gmail.com>
2022-10-04 14:48:59 +0530

    commit

---
+++ Exception.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T_CSharp
+{
+    public class task15
+    {
+        
+
+      
+        public void MultipleCatch()
+        {
+            int[] a = { 10, 8, 6, 4, 8, 2 };
+            int[] b = { 5, 0, 3, 0, 2 };
+            for (int i = 0; i < a.Length; i++)
+            {
+                try
+                {
+                    Console.WriteLine(a[i] / b[i]);
+                }
+                catch (DivideByZeroException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                catch (IndexOutOfRangeException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
+            }
+
+        }
+
+    }
+
+    internal class Exception
+    {
+        public static void Main()
+        {
+            task15 ex = new task15();
+         
+            Console.WriteLine("----------Multiple-Catch-----------");
+           
+            ex.MultipleCatch();
+        }
+    }
+}
---
+++ Validation.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T_CSharp
+{
+
+    public class UserValidate : ApplicationException
+    {
+        
+        public UserValidate(string message) : base(message)
+        {
+
+        }
+    }
+  
+    internal class Validation
+    {
+        public static void Main()
+        {
+            int total_trainees;
+           
+            Console.WriteLine("-----User Defined Validation-----");
+           
+            try
+            {
+                Console.WriteLine("Enter Your age");
+                total_trainees = Convert.ToInt32(Console.ReadLine());
+                if (total_trainees < 22)
+                {
+                    throw (new UserValidate("You are not eligible "));
+                }
+                else
+                {
+                    Console.WriteLine("You are Eligible");
+                }
+            }
+            catch (UserValidate e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
